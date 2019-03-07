931b8b05e189131fca30cac70b938ac9e1acf35b
Juho Friman <juho.t.friman@gmail.com>
2019-03-07 17:02:02 +0200

    Implemented super simple open file watcher

--- src/render/renderer.js
+++ src/render/renderer.js
@@ -9,13 +9,30 @@ function render() {
   content.innerHTML = md.render(data);
 }
 
+function readAndRender(path, cb) {
+  fs.readFile(path, 'utf8', function(err, file) {
+      data = file;
+      render();
+      cb();
+  });
+}
+
+ let watcher = null;
 
 window.eventbus.subscribe('OPEN_FILE', (payload) => {
   console.log('Opening file: ' + payload);
-  fs.readFile('/Users/juhofr/Documents/fireburn-docs/' + payload, 'utf8', function(err, file) {
-      data = file;
-      render();
+  if(watcher) {
+    watcher.close();
+  }
+  readAndRender('/Users/juhofr/Documents/fireburn-docs/' + payload, () => {
+    watcher = fs.watch('/Users/juhofr/Documents/fireburn-docs/' + payload, (eventType, filename) => {
+      console.log(eventType);
+      if(eventType === 'change') {
+        readAndRender('/Users/juhofr/Documents/fireburn-docs/' + payload);
+      }
+    });
   });
+
 });
 
 render();
