dfff5c0a3e7d5b1500049561bed843cf222adc0a
Juho Friman <juho.t.friman@gmail.com>
2019-03-07 16:48:13 +0200

    Nice, now we can open files and render them

--- src/render/file-listing.js
+++ src/render/file-listing.js
@@ -3,6 +3,11 @@ let data = [];
 
 var content = document.getElementById('file-listing');
 
+function openFile(file) {
+  console.log('Opening file: ' + file);
+  window.eventbus.publish('OPEN_FILE', file);
+}
+
 function render() {
   while (content.firstChild) {
     content.removeChild(content.firstChild);
@@ -10,6 +15,7 @@ function render() {
   data.forEach(file => {
     var message = document.createElement('div');
     message.innerHTML = file;
+    message.onclick = () => openFile(file);
     content.appendChild(message);
   });
 
--- src/render/renderer.js
+++ src/render/renderer.js
@@ -1,44 +1,21 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-function mdContent(...lines) {
-  return lines.join('\n');
-}
-
+const fs = require('fs');
 var MarkdownIt = require('markdown-it'),
     md = new MarkdownIt();
 
-var result = md.render(
-  mdContent(
-    '# The Awesome Markdown notebook',
-    'This could become my favourite markdown notebook.',
-    '',
-    'I tried to learn mindforger, but no luck. It was just over engineered, but had some good ideas.',
-    'Now, I try to make something similar, but easier and more human"',
-    '',
-    '> *Shut up and take my money!*',
-    '> Anon',
-    '',
-    '## Features',
-    '',
-    ' - Editing of content on the fly',
-    ' - Searching for matching content',
-    ' - Linking content',
-    ' - Nice layout',
-    ' - code highlight',
-    '',
-    ' ## Additional ideas',
-    ' - S3 sync',
-    ' - look and feel customisation',
-    '',
-    ' ### H3 level header',
-    '',
-    ' ### H4 level header',
-  )
-);
-var element = document.createElement('div');
-element.innerHTML = result;
-var content = document.getElementById('content');
+let data = '';
 
-content.appendChild(element);
+function render() {
+  var content = document.getElementById('content');
+  content.innerHTML = md.render(data);
+}
+
+
+window.eventbus.subscribe('OPEN_FILE', (payload) => {
+  console.log('Opening file: ' + payload);
+  fs.readFile('/Users/juhofr/Documents/fireburn-docs/' + payload, 'utf8', function(err, file) {
+      data = file;
+      render();
+  });
+});
+
+render();
